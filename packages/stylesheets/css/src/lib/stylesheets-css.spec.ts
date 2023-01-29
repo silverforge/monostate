1cf138b7b1a661cf9dd885060075bbe58539826f
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2023-01-29 12:54:02 -0500

    bye bye tailwind

--- packages/stylesheets/css/src/lib/stylesheets-css.spec.ts
+++
@@ -1,7 +0,0 @@
-import { stylesheetsCss } from './stylesheets-css';
-
-describe('stylesheetsCss', () => {
-  it('should work', () => {
-    expect(stylesheetsCss()).toEqual('stylesheets-css');
-  });
-});
--- packages/stylesheets/css/src/lib/stylesheets-css.ts
+++
@@ -1,3 +0,0 @@
-export function stylesheetsCss(): string {
-  return 'stylesheets-css';
-}
