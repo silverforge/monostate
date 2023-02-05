11de41267b2b102187feab23b7a962a8b80425db
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2023-02-05 07:38:49 -0500

    icons added

--- apps/blog-mono/src/app/features/post-list/post-list-item.tsx
+++ apps/blog-mono/src/app/features/post-list/post-list-item.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Post } from "../../../typedefs";
 import { Card } from "@monostate/components";
+import { DeleteIcon, EditIcon } from "@monostate/icons";
 
 import styles from './post-list-item.module.css';
 
@@ -12,7 +13,13 @@ export const PostListItem: FC<PostListItemProps> = ({ post }) => {
   return (
     <Card>
       <div className={styles['post-list-item']}>
-        <span className={styles['post-list-item__title']} >{post.title}</span>
+        <div className={styles['post-list-item__header-box']}>
+          <span className={styles['post-list-item__title']} >{post.title}</span>
+          <div className={styles['post-list-item__icon-box']}>
+            <EditIcon />
+            <DeleteIcon />
+          </div>
+        </div>
         <span className={styles['post-list-item__text']}>{post.text}</span>
       </div>
     </Card>
---
+++ packages/components/src/icons/index.ts
@@ -0,0 +1,2 @@
+export * from './edit-icon';
+export * from './delete-icon';
