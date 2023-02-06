750dcee9f1e124a2bc1316d9cb79b41c0c46c7e8
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2023-02-05 20:19:47 -0500

    delete post dialog extracted

---
+++ apps/blog-mono/src/app/features/dialog/delete-post-dialog.tsx
@@ -0,0 +1,40 @@
+import styles from './delete-post-dialog.module.css';
+import { Button, Card, Dialog } from "@monostate/components";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { postListActions } from "../post-list/post-list-slice";
+import { deletePostAsync } from "../../store/thunks/deletePostAsync";
+import { useState } from 'react';
+
+export const DeletePostDialog = () => {
+  const dispatch = useAppDispatch();
+  const isDeleteDialogOpen = useAppSelector(state => state.postList.isDeleteDialogOpen);
+  const markedAsDeletedId = useAppSelector(state => state.postList.markedAsDeletedId);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onOkClick = async () => {
+    setIsLoading(true);
+    await dispatch(deletePostAsync({ id: markedAsDeletedId }));
+    dispatch(postListActions.closeDeleteDialog());
+    setIsLoading(false);
+  }
+
+  const onCancelClick = () => {
+    dispatch(postListActions.closeDeleteDialog());
+  }
+
+  return (
+    <Dialog isOpen={isDeleteDialogOpen}>
+      <Card>
+        <div className={styles['delete-dialog__container']}>
+          <span>Are you sure you want to delete this post?</span>
+          {isLoading && <div>Deleting post...</div>}
+          <div className={styles['button-box']}>
+            <Button onClick={onOkClick} text="Ok" />
+            <Button onClick={onCancelClick} text="Cancel" />
+          </div>
+        </div>
+      </Card>
+    </Dialog>
+  );
+}
--- apps/blog-mono/src/app/features/post-list/post-list.tsx
+++ apps/blog-mono/src/app/features/post-list/post-list.tsx
@@ -1,31 +1,16 @@
-import { Button, Card, Dialog, LoadingScreen } from "@monostate/components";
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import styles from './post-list.module.css';
+
+import { LoadingScreen } from "@monostate/components";
+import { useAppSelector } from "../../store/hooks";
 import { useGetPostListAsync } from "./hooks/useGetPostListAsync";
 import { PostListItem } from "./post-list-item";
-
-import styles from './post-list.module.css';
-import { postListActions } from "./post-list-slice";
-import { deletePostAsync } from "../../store/thunks/deletePostAsync";
+import { DeletePostDialog } from "../dialog/delete-post-dialog";
 
 export const PostList = () => {
-  const dispatch = useAppDispatch();
   const isPostListLoading = useAppSelector(state => state.postList.isPostListLoading);
-  const isDeleteDialogOpen = useAppSelector(state => state.postList.isDeleteDialogOpen);
-  const markedAsDeletedId = useAppSelector(state => state.postList.markedAsDeletedId);
   const { posts } = useGetPostListAsync();
 
   if (isPostListLoading) return <LoadingScreen />
-
-  const onOkClick = async () => {
-    console.log(' ::: ok ::: ');
-    await dispatch(deletePostAsync({ id: markedAsDeletedId }));
-    dispatch(postListActions.closeDeleteDialog());
-  }
-
-  const onCancelClick = () => {
-    console.log(' ::: cancel ::: ');
-    dispatch(postListActions.closeDeleteDialog());
-  }
 
   return (
     <>
@@ -35,17 +20,7 @@ export const PostList = () => {
         ))}
       </div>
 
-      <Dialog isOpen={isDeleteDialogOpen}>
-        <Card>
-          <div className={styles['delete-dialog__container']}>
-            <span>Are you sure you want to delete this post?</span>
-            <div className={styles['button-box']}>
-              <Button onClick={onOkClick} text="Ok" />
-              <Button onClick={onCancelClick} text="Cancel" />
-            </div>
-          </div>
-        </Card>
-      </Dialog>
+      <DeletePostDialog />
     </>
   );
 }
